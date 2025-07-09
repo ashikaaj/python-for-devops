515732e1c5e4132c858c44ab2814e9ebb163f652
ashikaaj <133088160+ashikaaj@users.noreply.github.com>
2025-07-09 21:50:18 +0530

    Update 03-regex-findall.py

--- Day-02/examples/03-regex-findall.py
+++ Day-02/examples/03-regex-findall.py
@@ -5,6 +5,6 @@ pattern = r"brown"
 
 search = re.findall(pattern, text)
 if search:
-    print("Pattern found:", search)
+    print("Pattern found:", search[0])
 else:
     print("Pattern not found")
